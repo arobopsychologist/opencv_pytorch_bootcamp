736bc0a604f192d930b9f33a165aacbf5afba5ad
arobopsychologist <edwardjlsh@pm.me>
2025-06-29 12:45:15 +0200

    Add dependencies

---
+++ scripts/get_data.sh
@@ -0,0 +1,4 @@
+mkdir -p data
+
+wget -q "https://learnopencv.com/wp-content/uploads/2024/07/mnist_0.jpg" -O "data/mnist_0.jpg"
+wget -q "https://learnopencv.com/wp-content/uploads/2024/07/mnist_1.jpg" -O "data/mnist_1.jpg"
